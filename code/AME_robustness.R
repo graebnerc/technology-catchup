82e2d1487faf5bb62f878d4f521dc18b2fdcd9a2
Claudius Graebner <claudius.graebner@posteo.de>
2021-08-16 18:25:03 +0200

    Add code for more elaborate robustness checks

---
+++ code/AME_robustness.R
@@ -0,0 +1,258 @@
+suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library(data.table))
+suppressPackageStartupMessages(library(here))
+suppressPackageStartupMessages(library(countrycode))
+suppressPackageStartupMessages(library(lmtest))
+suppressPackageStartupMessages(library(sandwich))
+suppressPackageStartupMessages(library(stargazer))
+suppressPackageStartupMessages(library(plm))
+suppressPackageStartupMessages(library(margins))
+suppressPackageStartupMessages(library(icaeDesign))
+source(here("code/regressions_funs.R"))
+
+redo_computation <- TRUE
+variable_of_interest <- "eci"#  "eci" # "GDP_pc_PPP_log" 
+vary_only_start <- TRUE # varies only start years and runs always until 2016
+if (vary_only_start){
+  analysis_result_file <- here(
+    paste0("output/robustness/estimations_", variable_of_interest, "_red.csv"))
+  analysis_result_file_mpdata <- here(
+    paste0("output/robustness/mpdata_", variable_of_interest, "_red.csv"))
+  pdf_file_name <- here(paste0(
+    "output/robustness/robustness_check_red.pdf"))
+  txt_file <- here(paste0("output/opec/period_info_red.txt"))
+} else {
+  analysis_result_file <- here(
+    paste0("output/robustness/estimations_", variable_of_interest, ".csv"))
+  analysis_result_file_mpdata <- here(
+    paste0("output/robustness/mpdata_", variable_of_interest, ".csv"))
+  pdf_file_name <- here(paste0(
+    "output/robustness/opec_shit.pdf"))
+  txt_file <- here(paste0("output/opec/period_info.txt"))
+}
+
+if (redo_computation){
+  # 1.1. Data setup------------------------------
+  countries_considered <- c(
+    'ALB', 'ARG', 'AUS', 'AUT', 'BDI', 'BEL', 'BEN', 'BFA', 'BGR', 'BOL', 'BRA', 
+    'BRB', 'CAF', 'CAN', 'CHE', 'CHL', 'CHN', 'CIV', 'CMR', 'COG', 'COL', 'CRI', 
+    'CYP', 'DEU', 'DNK', 'DOM', 'DZA', 'ECU', 'EGY', 'ESP', 'ETH', 'FIN', 'FJI', 
+    'FRA', 'GAB', 'GBR', 'GHA', 'GMB', 'GRC', 'GTM', 'HKG', 'HND', 'HTI', 'HUN', 
+    'IDN', 'IND', 'IRL', 'IRQ', 'ISL', 'ISR', 'ITA', 'JAM', 'JOR', 'JPN', 'KEN', 
+    'KHM', 'KOR', 'KWT', 'LAO', 'LBR', 'MAR', 'MDG', 'MDV', 'MEX', 'MLI', 'MLT', 
+    'MMR', 'MNG', 'MRT', 'MUS', 'MWI', 'MYS', 'NER', 'NGA', 'NIC', 'NLD', 'NOR', 
+    'NPL', 'NZL', 'PAK', 'PAN', 'PER', 'PHL', 'POL', 'PRT', 'PRY', 'RWA', 'SAU', 
+    'SDN', 'SEN', 'SGP', 'SLE', 'SLV', 'SWE', 'SYR', 'TGO', 'THA', 'TTO', 'TUN', 
+    'TUR', 'TZA', 'UGA', 'URY', 'USA', 'VEN', 'VNM', 'ZAF', 'ZMB')
+  
+  # ADJUST THESE PARAMETERS
+  vars_used <- c(
+    "AdvancedCountry", "HighIncome", "LowIncome", "LowerMiddleIncome", 
+    "HigherMiddleIncome", "OPECdummy",
+    "inv_share" # TODO <- Not yet considered
+  )
+  
+  vars_inst <- c("political_rel", "economic_rel", "legal_rel", "PropertyRights")
+  
+  vars_used_avg <- c(
+    "ccode", 
+    "eci", "Penn_GDP_PPP_log","GDP_pc_growth", "KOF_econ", "pop_growth", "hc",
+    "primary_exports_1_share_country", "oil_exports_share_country", 
+    "coal_and_metal_exports_share_country")
+  
+  total_vars <- c(
+    "Year", "Penn_GDP_PPP", "population", "GNIpc", "PropertyRights", "period",
+    "propertyrights", "politicalquality", "economicquality", "legalquality",
+    vars_used_avg, vars_used, vars_inst)
+  
+  threshold_high_income <- 12055
+  threshold_low_income <- 995.99
+  threshold_lowermiddle_income <- 3895
+  
+  #' Create data for cross-country growth regressions
+  #' 
+  #' @param init_data The raw data
+  #' @param start_date The first year to be considered
+  #' @param end_date The last year to be considered
+  #' @return data.frame
+  get_reg_data <- function(init_data, start_date, end_date){
+    # ADD PARAMETERS
+    
+    # ACTUAL START
+    data_reg_firstyear <- init_data %>%
+      filter(Year==start_date) %>%
+      select(all_of(c(vars_used_avg, vars_used)))
+    
+    data_reg_first_last <- init_data %>%
+      filter(Year>=start_date, Year<=end_date) %>%
+      select(all_of(c(vars_used, vars_used_avg, "period")))
+    
+    regression_data_1_year <- init_data %>%
+      filter(Year>=start_date, Year<=end_date) %>%
+      select(all_of(c(vars_used, vars_used_avg, "Year")))
+    
+    data_reg_first_last_AVG <- data_reg_first_last %>% 
+      dplyr::group_by(ccode) %>% 
+      dplyr::summarise(
+        across(
+          .cols = where(is.double), .fns = mean, na.rm=T, .names = "{.col}_Mean"), 
+        .groups = "drop") %>%
+      dplyr::select(all_of(
+        c("ccode", "pop_growth_Mean", "hc_Mean", 
+          "primary_exports_1_share_country_Mean",
+          "oil_exports_share_country_Mean", 
+          "coal_and_metal_exports_share_country_Mean",
+          "GDP_pc_growth_Mean", "KOF_econ_Mean", "pop_growth_Mean", "hc_Mean",
+          "inv_share_Mean")))
+    
+    data_reg_predict <- data_reg_firstyear %>%
+      select(all_of(
+        c("ccode", "eci", "Penn_GDP_PPP_log", "AdvancedCountry", "HighIncome", 
+          "LowIncome", "LowerMiddleIncome", "HigherMiddleIncome", "OPECdummy")
+      )) %>%
+      inner_join(., data_reg_first_last_AVG, by=c("ccode"))%>%
+      dplyr::rename(
+        avg_GDP_pc_PPP_growth=GDP_pc_growth_Mean,
+        kof_econ=KOF_econ_Mean,
+        GDP_pc_PPP_log=Penn_GDP_PPP_log,
+        popgrowth=pop_growth_Mean,
+        humancapital=hc_Mean,
+        inv_share=inv_share_Mean,
+        primaryexports=primary_exports_1_share_country_Mean,
+        oilexports=oil_exports_share_country_Mean,
+        coalandmetalexports=coal_and_metal_exports_share_country_Mean
+      )
+  }
+  
+  base_data_reg <- data.table::fread(here("data/ECI-growth-data_final.csv")) %>%
+    select(any_of(total_vars)) %>%
+    mutate(
+      Penn_GDP_PPP = Penn_GDP_PPP * 1000000000,
+      Penn_GDP_PPP_log = log(Penn_GDP_PPP),
+      log_pop = log(population),
+      oil_exports_share_country = oil_exports_share_country*100, 
+      coal_and_metal_exports_share_country = coal_and_metal_exports_share_country*100,
+      HighIncome = ifelse(GNIpc>threshold_high_income, 1, 0),
+      LowIncome = ifelse(GNIpc<=threshold_low_income, 1, 0),
+      LowerMiddleIncome = ifelse(
+        GNIpc>threshold_low_income & GNIpc<=threshold_lowermiddle_income, 1, 0),
+      HigherMiddleIncome = ifelse(
+        GNIpc>threshold_lowermiddle_income & GNIpc<=threshold_high_income, 1, 0)
+    ) %>%
+    filter(
+      ccode %in% countries_considered
+    ) %>%
+    group_by(ccode) %>%
+    mutate(
+      (pop_growth = log_pop - dplyr::lag(log_pop))*100
+    ) %>%
+    ungroup() 
+  
+  
+  min_range <- 1962:2012
+  # min_range <- 1980:1981
+  # min_range <- 2010:2012
+  full_dat_list <- list()
+  
+  for (mi in min_range){
+    if (vary_only_start){
+      max_range <- 2016
+    } else{
+      max_range <- seq(mi+4, 2016)
+    }
+    for (ma in max_range) {
+      print(paste(mi, "-", ma))
+      
+      reg_res <- tryCatch(expr = {
+        
+        current_data <- get_reg_data(
+          init_data = base_data_reg, 
+          start_date = mi, 
+          end_date = ma)
+        
+        regr_form <- as.formula(
+          paste0("avg_GDP_pc_PPP_growth ~ GDP_pc_PPP_log + kof_econ + ", 
+                 "eci*GDP_pc_PPP_log + popgrowth + humancapital + inv_share + OPECdummy*eci")
+        )
+        
+        reg_man <- lm(formula = regr_form, data=current_data, na.action=na.exclude)
+        
+        cplot_data_eci <- cplot(
+          object =reg_man, 
+          dx = "eci", 
+          x = "GDP_pc_PPP_log", 
+          data = current_data,  
+          what = "effect", 
+          draw = FALSE,
+          xvals = seq(6, 12, 0.1)
+        )
+        
+        cplot_data_gdp <- cplot(
+          object =reg_man, 
+          dx = "GDP_pc_PPP_log", 
+          x = "eci", 
+          data = current_data,  
+          what = "effect", 
+          draw = FALSE,
+          xvals = seq(-2.4, 2.4, 0.1)
+        )
+        
+        cplot_data_eci_final <- tibble(cplot_data_eci) %>%
+          mutate(dx_variable="eci") 
+        cplot_data_gdp_final <- tibble(cplot_data_gdp) %>%
+          mutate(dx_variable="GDP") 
+        cplot_data_final <- rbind(
+          cplot_data_eci_final, cplot_data_gdp_final) %>%
+          mutate(period=paste(mi, "-", ma),
+                 first_year = mi, last_year = ma)
+      }, 
+      error=function(cond) {
+        message(paste("Error with estimation for the period:", mi, "-", ma))
+        message("Here's the original error message:")
+        message(cond)
+        cplot_data_final <- NA
+      })
+      full_dat_list[[paste0(mi, "_", ma)]] <- reg_res
+    }
+  }
+  red_dat_list <- full_dat_list[!is.na(full_dat_list)]
+  full_plot_list <- rbindlist(red_dat_list)
+  fwrite(full_plot_list, file = here("output/robustness/robustness_est.csv"))
+}
+
+# Visualization----------------------------------
+
+plot_data <- fread(here("output/robustness/robustness_est.csv"))
+
+robustness_gdp <- plot_data %>%
+  filter(first_year<2010, dx_variable=="eci") %>%
+  ggplot(., aes(x=xvals, y=yvals, group=period)) +
+  geom_line(color="grey", alpha=0.75) + 
+  labs(
+    title = "AME of initial GDP",
+    x = "ECI", y = "AME of initial GDP per capita"
+  ) +
+  theme_icae()
+
+robustness_eci <- plot_data %>%
+  filter(first_year<2010, dx_variable=="GDP") %>%
+  ggplot(., aes(x=xvals, y=yvals, group=period)) +
+  geom_line(color="grey", alpha=0.75) + 
+  labs(
+    title = "AME of ECI",
+    x = "Initial GDP per capita", y = "AME of ECI"
+  ) +
+  theme_icae()
+
+robustness_full <- ggpubr::ggarrange(
+  robustness_eci, robustness_gdp, ncol = 2, labels = c("A)", "B)")
+)
+
+robustness_full <- ggpubr::annotate_figure(
+  robustness_full, 
+  top = ggpubr::text_grob("Average marginal effects for varying starting years", size = 14))
+
+ggsave(plot = robustness_full, 
+       filename = here("output/fig_4_robustness.pdf"), 
+       width = 9, height = 4)
+
