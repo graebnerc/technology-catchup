bfe3ed5ced35a1f4ad6cc75c4ab078bb25f32fae
Claudius Graebner <claudius.graebner@posteo.de>
2021-08-16 18:25:28 +0200

    Merge figs 2 and 3, and switch color scheme to brewer

--- code/fig_2_3_margins.R
+++ code/fig_2_3_margins.R
@@ -5,29 +5,31 @@ suppressPackageStartupMessages(library(here))
 options(warn = -1)
 load(here("data/intermediate_data.RData"))
 
+data_reg_predict_1985_2014_use <- data_reg_predict_1985_2014 %>%
+  rename(ECI=eci, 
+         `GDP pc (PPP, log)` = GDP_pc_PPP_log)
 # Figure 2 ---------
 # Marginal effects at different levels of initial technological capabilities
 
 reg_predict_7 <- lm(
-  avg_GDP_pc_PPP_growth ~ GDP_pc_PPP_log + kof_econ + eci*GDP_pc_PPP_log + 
-    popgrowth + humancapital, 
-  data=data_reg_predict_1985_2014, na.action=na.exclude)
+  avg_GDP_pc_PPP_growth ~ `GDP pc (PPP, log)` + kof_econ + 
+    ECI*`GDP pc (PPP, log)` + popgrowth + humancapital + inv_share, 
+  data=data_reg_predict_1985_2014_use, na.action=na.exclude)
 
 #note: ci.lvl = ... sets the confidence interval (outer probability or high 
 # density probability; see package documentation sjplot)
 
 fig_2 <- plot_model(
   reg_predict_7, type = "pred", 
-  terms = c("GDP_pc_PPP_log", "eci [-2, 0, 2]"), ci.lvl=0.9) + 
+  terms = c("GDP pc (PPP, log)", "ECI [-2, 0, 2]"), ci.lvl=0.9) + 
   font_size(title = 12) +     
   labs(
-    y="Predicted average growth in GDP per capita (1985-2014)",
+    y="Predicted average growth in GDP pc (1985-2014)",
     x="GDP per capita in 1985 (in logs)",
-    title="Predicted values of average GDP per capita growth over 1985-2014"
+    title="Initial level of Economic Complexity"
   ) +
   theme_bw() +
-  scale_color_viridis_d() +
-  scale_fill_viridis_d() +
+  scale_color_brewer(palette = "Dark2", aesthetics = c("color", "fill")) +
   scale_x_continuous(expand = expansion(add = c(0, 0.1))) +
   scale_y_continuous(expand = expansion()) +
   theme(
@@ -39,26 +41,22 @@ fig_2 <- plot_model(
     axis.text.y = element_text(size = 8), 
     legend.position = "bottom")
 fig_2
-ggsave(plot = fig_2, 
-       filename = here("output/fig_2-marginal-effects.pdf"), 
-       width = 6, height = 4)
 
 # Figure 3 ----
 # Figure 3: Marginal effects at different levels of initial GDP per capita
 
 fig_3 <- plot_model(
   reg_predict_7, type = "pred", 
-  terms = c("eci", "GDP_pc_PPP_log [7, 8.5, 10]"), 
+  terms = c("ECI", "GDP pc (PPP, log) [7, 8.5, 10]"), 
   ci.lvl=0.9) +
   font_size(title = 12) +     
   labs(
-    y="Predicted average growth in GDP per capita (1985-2014)",
+    y="Predicted average growth in GDP pc (1985-2014)",
     x="Economic Complexity Index in 1985",
-    title="Predicted values of average GDP per capita growth over 1985-2014"
+    title="Initial level of GDP"
   ) +
   theme_bw() +
-  scale_color_viridis_d() +
-  scale_fill_viridis_d() +
+  scale_color_brewer(palette = "Dark2", aesthetics = c("color", "fill")) +
   scale_x_continuous(expand = expansion(add = c(0, 0.1))) +
   scale_y_continuous(expand = expansion()) +
   theme(
@@ -71,6 +69,13 @@ fig_3 <- plot_model(
     legend.position = "bottom")
 fig_3
 
-ggsave(plot = fig_3, 
-       filename = here("output/fig_3-marginal-effects.pdf"), 
-       width = 6, height = 4)
+fig23 <- ggpubr::ggarrange(
+  fig_2, fig_3, ncol = 2, labels = c("A)", "B)"), 
+  common.legend = FALSE, legend = "bottom")
+
+fig23 <- ggpubr::annotate_figure(fig23,
+  top = ggpubr::text_grob("Predicted average GDP pc growth over 1985-2014", size = 14))
+
+ggsave(plot = fig23, 
+       filename = here("output/fig_23-predictions.pdf"), 
+       width = 9, height = 4)
